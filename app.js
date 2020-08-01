51289d1a4ce608d8d4f364e297be7d3eb189870c
sudharshan <sudharshanbhimisetty24@gmail.com>
2020-08-01 11:50:50 +0000

    running files ic completed

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res){
+	res.render("index");
+})
+
+
+
+app.listen(3000, function(){
+   console.log("The beatword Server Has Started!");
+});
